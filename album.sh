3988da2b0d75077ff635d42c716d9ee9ac72642e
Doug Smith <dsmith47@nd.edu>
2019-10-15 01:46:27 -0400

    Fixed bug in script for passing on argument values

--- album.sh
+++ album.sh
@@ -3,6 +3,6 @@ youtube-dl \
   --extract-audio \
   --audio-format mp3 \
   -o "output/%(title)s.%(ext)s" \
-  $6
+  "$6"
 
-python3 metadata.py3 $1 $2 $3 $4 $5
+python3 metadata.py3 "$1" "$2" "$3" "$4" "$5"
